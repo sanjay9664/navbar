f6b09981bd5833b87089c969ab4d38bfd2db7930
sanjay gupta <guptaji30749@gmail.com>
2022-11-24 09:13:27 +0530

    navbar

---
+++ component/navbar.js
@@ -0,0 +1,18 @@
+function navbar()
+{
+  return`
+   <div id="navbar">
+  <div id="home">
+    <li>Home</li>
+  </div>
+
+  <div id="anothe">
+    <li>About</li>
+    <li>Email</li>
+    <li>connect</li>
+    <li>Help</li>
+  </div>
+</div>`
+}
+
+export default navbar
